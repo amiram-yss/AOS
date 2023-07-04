a9655d33827ef4a534ebffc86bc917befa6eaa28
Amiram <amiramyss@gmail.com>
2023-07-05 01:00:39 +0300

    Added custom segmentation addresses. Changed ORG to 0. Added jmp 0x7c0:BOOT in order to define code segmentation in 0x7c0.

--- boot.asm
+++ boot.asm
@@ -1,7 +1,20 @@
-ORG 0x7C00
+;ORG 0x7C00 obselete
+ORG 0 ;origin 0
 BITS 16
 
+jmp 0x7c0:BOOT
+
 BOOT:
+    CLI; Critical code here, avoiding interrupts.
+
+    mov ax, 0x00 ; overriding BIOS's STACK seg
+    mov ss, ax ; stack segmentation lowest add
+    mov sp, 0x7c00 ; stack pointer at the bottom of the stack
+    mov ax, 0x7c0 ;overriding BIOS's segmentations DATA, EXTRA segs
+    mov ds, ax ;data seg at 0x7c00
+    mov es, ax ;extra seg at 0x7c00
+
+    STI; Returning interrupts. The code is no longer critical
     MOV BX, MESSAGE
     CALL PRINTS
     JMP $
@@ -23,7 +36,7 @@ PRINTC:
     INT 0x10
     RET
 
-MESSAGE: DB 'Booting system...'
+MESSAGE: DB 'AOS Booting...'
 
 TIMES 510 - ($ - $$) DB 0
 DW 0xAA55
